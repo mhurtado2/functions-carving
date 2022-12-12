1fe7c308bb1988d8d9efb1b51c157cc03bbaae88
Shane <mhurtado1@berklee.edu>
2022-12-12 09:31:58 -0600

    func-carving

---
+++ main.js
@@ -0,0 +1,27 @@
+const createWoodBlock = () => {
+    // Return an object with 4 properties. 
+   return {
+    type: "wood block",
+    length: 10,
+    material: "pine",
+    purpose: "flute"
+   }
+}
+
+//console.log(createWoodBlock())
+
+
+const createBeautifulCarving = (woodBlock) => {
+    // Return a string representation of the object
+    return `The ${woodBlock.length}-inch, ${woodBlock.material} woodblock was carved into a wooden ${woodBlock.purpose}`
+}
+
+
+// The function returns a value, so that value is stored in a variable
+const woodBlock = createWoodBlock()
+
+// The function returns a value, so that value is stored in a variable
+const carvingString = createBeautifulCarving(woodBlock)  // <-- Missing an argument. Fix it.
+
+console.log(carvingString)
+
